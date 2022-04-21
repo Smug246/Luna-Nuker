a454bfbae3068f7024c9d3eb9c64e6da80a080af
Smug <99215486+Smug246@users.noreply.github.com>
2022-04-21 22:55:56 +0100

    Add files via upload

---
+++ lunaconfig.py
@@ -0,0 +1,11 @@
+# replace "paste here 1" with your bot token.
+# replace "paste here 2" with your discord id. Example: 500755993374752789
+# replace "paste here 3" with your custom bot status. Example: luna's goated!
+# replace "paste here 4" with the name you want the server you nuke to be change to. Example: ez nuke by luna
+# replace "paste here 5" with different names you want to appear on the channels. Example: ["luna nuked you", "faggots", "luna owns you"]
+
+token = "ODMxODUxOTMwMjA0NjM1MTQ3.YHbQew.9s3YbAXBkHnz3Ps2WQNySIuwQWk"
+username = "500755993374752789"
+bot_status = "suck your mum"
+server_name = "luna raped you"
+channel_name_list = ["luna fucked you" , "faggots" , "luna owns you"]
